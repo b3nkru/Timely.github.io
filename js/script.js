5a77c0740b26c53908ed0deb193d8e39bb8408e3
Ben Kruseski <72771152+BKruseski@users.noreply.github.com>
2024-11-20 08:49:08 -0500

    Login and Home page

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const errorMessage = document.getElementById("errorMessage");
+
+    if(username === "admin" && password === "password123") {
+        window.location.href = "home.html";
+    } else {
+        errorMessage.textContent = "Invalid username or password. Please try again.";
+    }
+});
